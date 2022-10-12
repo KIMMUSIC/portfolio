77c7d6c65c2504db873eccdda3167875f81fc32a
KIMMUSIC <ldgyuuuuu@gmail.com>
2022-10-12 13:57:24 +0900

    router

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import './App.css';
 import HomePage from './pages/HomePage';
 import Project from "./pages/Project";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
