13528d1010fa1959d88f88e0416924136d25365a
KIMMUSIC <ldgyuuuuu@gmail.com>
2022-10-14 14:09:23 +0900

    second

--- src/compounds/ProjectsCompound.js
+++ src/compounds/ProjectsCompound.js
@@ -1,13 +1,16 @@
 import React from "react";
 import BodyWrapper from "../components/Body/BodyWrapper";
 import Project1 from "../components/Projects/Project1";
+import Project2 from "../components/Projects/Project1";
 
 
 
 function ProjectsCompound({ children, ...restProps}) {
 
   return (
+  <>
         <BodyWrapper className = "projects"><Project1></Project1></BodyWrapper>
+        </>
   );
 }
 
